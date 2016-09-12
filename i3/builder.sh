4fd52d738c78a302dde3d2ca32ce02f7af1d873f
Xavier Detant <xavier.detant@gmail.com>
2016-09-13 00:01:59 +0200

    Configure i3 but request to run a script by the user

--- i3/builder.sh
+++ i3/builder.sh
@@ -2,5 +2,8 @@ source $stdenv/setup
 
 # mkdir -p $home/.config/i3
 # cp config $home/.config/i3/
-mkdir $out
-cp $src/config $out
+mkdir -p $out/i3 $out/bin
+cp $src/config $out/i3
+echo "ln -f -s ~/.nix-profile/i3 -t ~/.config" >> $out/bin/updateNixLinks
+chmod +x $out/bin/updateNixLinks
+
