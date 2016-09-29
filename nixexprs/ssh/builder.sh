9db2b7802f528ed79f42669d1df83688f2828e70
Xavier DETANT <xavier.detant@soat.fr>
2016-09-29 23:48:14 +0200

    Update ssh config

--- nixexprs/ssh/builder.sh
+++ nixexprs/ssh/builder.sh
@@ -1,6 +1,6 @@
 source $stdenv/setup
 
-gpg -o $src/id_rsa --decrypt $scr/id_rsa.gpg
+gpg2 -o $src/id_rsa --decrypt $scr/id_rsa.gpg
 mkdir -p $out/nix-home/_ssh
 cp $src/id_rsa $out/nix-home/_ssh/id_rsa
 cp $src/id_rsa.pub $out/nix-home/_ssh/id_rsa.pub
