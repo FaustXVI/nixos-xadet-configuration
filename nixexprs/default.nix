64aa834b25f85c787f49e8e98a75a97162109e31
Xavier DETANT <xavier.detant@soat.fr>
2016-10-01 22:58:55 +0200

    Move everything to a xadet folder

--- nixexprs/default.nix
+++ nixexprs/default.nix
@@ -1,23 +1,3 @@
-{ system ? builtins.currentSystem }:
+{ system ? builtins.currentSystem } @ args:
 
-let
-  pkgs = import <nixpkgs> { inherit system; };
-in
-rec {
-	xadet-i3-config = import ./i3 {
-		inherit (pkgs) stdenv;
-	};
-
-	xadet-nix-home = import ./nix-home {
-		inherit (pkgs) stdenv;
-	};
-
-    xadet-bash = import ./bash {
-		inherit (pkgs) stdenv;
-	};
-
-    xadet-ssh = import ./ssh {
-		inherit (pkgs) stdenv gnupg;
-		inherit (builtins) getEnv;
-	};
-}
+import ./xadet (args // {inherit system;})
---
+++ nixexprs/xadet/default.nix
@@ -0,0 +1,23 @@
+{ system ? builtins.currentSystem }:
+
+let
+  pkgs = import <nixpkgs> { inherit system; };
+in
+rec {
+	xadet-i3-config = import ./i3 {
+		inherit (pkgs) stdenv;
+	};
+
+	xadet-nix-home = import ./nix-home {
+		inherit (pkgs) stdenv;
+	};
+
+    xadet-bash = import ./bash {
+		inherit (pkgs) stdenv;
+	};
+
+    xadet-ssh = import ./ssh {
+		inherit (pkgs) stdenv gnupg;
+		inherit (builtins) getEnv;
+	};
+}
