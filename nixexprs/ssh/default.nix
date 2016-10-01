1c2bcf880163046945649cd8b5eb48ba4f64d035
Xavier DETANT <xavier.detant@soat.fr>
2016-10-01 22:20:48 +0200

    Ssh key can be install with environment variable

--- nixexprs/ssh/default.nix
+++ nixexprs/ssh/default.nix
@@ -1,9 +1,9 @@
-{ stdenv, gnupg }:
+{ stdenv, gnupg, getEnv }:
 
 stdenv.mkDerivation {
 	name = "xadet-ssh-1.0";
 	builder = ./builder.sh;
 	src = ./.;
 	buildInputs = [ gnupg ];
-	impureEnvVars = [ "passphrase" ];
+	PASSPHRASE = getEnv "PASSPHRASE";
 }
