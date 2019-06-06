c7ded19df34c2ca828c21b07fbf71eea01d78fb3
FaustXVI <xavier.detant@gmail.com>
2019-06-06 14:47:42 +0200

    Add git alias for live coding

--- pkgs/git.nix
+++ pkgs/git.nix
@@ -14,6 +14,7 @@
                 st = "status";
                 br = "branch";
                 branch-clean = "!git branch --merged | grep -v master | xargs -n 1 git branch -d";
+                next = "!git checkout `git rev-list HEAD..demo-end | tail -1`";
             };
         };
     };
