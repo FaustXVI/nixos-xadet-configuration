8af71d3881db63d558a03d1f06352e3242e482e6
FaustXVI <xavier.detant@gmail.com>
2019-08-08 22:01:41 +0200

    Add global ignore file

--- pkgs/git.nix
+++ pkgs/git.nix
@@ -15,6 +15,11 @@
                 br = "branch";
                 branch-clean = "!git branch --merged | grep -v master | xargs -n 1 git branch -d";
                 next = "!git checkout `git rev-list HEAD..demo-end | tail -1`";
+              };
+            extraConfig = {
+              core = {
+                excludesfile = "~/.gitignore_global";
+              };
             };
         };
     };
