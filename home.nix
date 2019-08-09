4fda7c78808e1774c8dca39761cca72c55f898f3
FaustXVI <xavier.detant@gmail.com>
2019-08-09 14:44:11 +0200

    Manage home-manager through channels

--- home.nix
+++ home.nix
@@ -36,10 +36,4 @@ with pkgs; {
     enableSshSupport = true;
     enableExtraSocket = true;
   };
-  programs = {
-    home-manager = {
-      enable = true;
-      path = https://github.com/rycee/home-manager/archive/master.tar.gz;
-    };
-  };
 }
