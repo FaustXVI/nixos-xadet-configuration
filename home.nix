91fd1cde4e4bee97ef57acd7054b51c8bf3f4103
FaustXVI <xavier.detant@gmail.com>
2019-08-23 15:41:13 +0200

    Better fish

--- home.nix
+++ home.nix
@@ -29,6 +29,8 @@ with pkgs; {
       autoPatchelfHook
       srm
       dia
+      fzf
+      gitAndTools.hub
   ];
   home.file.".nix-channels".source = ./nix-channels;
   services.gpg-agent = {
--- pkgs/key_bindings.fish
+++ pkgs/key_bindings.fish
@@ -1,6 +1,3 @@
-bind \cr 'peco_select_history (commandline -b)'
-bind \ch 'fuck'
-bind \cs sudope
 bind ! __history_previous_command
 bind '$' __history_previous_command_arguments
 
